86065c0f90e98fee577bd26deca9041e194bda89
Ivan Alejandro <ivanalejandro0@gmail.com>
2023-09-11 12:00:43 -0300

    Add some Telescope mappings

--- lua/ivan/plugins/fzf.lua
+++ lua/ivan/plugins/fzf.lua
@@ -12,10 +12,10 @@ return {
     vim.g.fzf_buffers_jump = 1
 
     vim.keymap.set("n", ",e", ":Files<CR>", { desc = "Fuzzy find files" })
-    vim.keymap.set("n", ",b", ":Buffers<CR>", { desc = "Fuzzy find open buffers" })
+    -- vim.keymap.set("n", ",b", ":Buffers<CR>", { desc = "Fuzzy find open buffers" })
     vim.keymap.set("n", ",g", ":BTags<CR>", { desc = "Fuzzy find buffer tags" })
     vim.keymap.set("n", ",f", ":BLines<CR>", { desc = "Fuzzy find buffer lines" })
-    vim.keymap.set("n", ",m", ":History<CR>", { desc = "Fuzzy find history" })
+    -- vim.keymap.set("n", ",m", ":History<CR>", { desc = "Fuzzy find history" })
     vim.keymap.set("n", "<Leader>f", ":Filetypes<CR>", { desc = "Fuzzy find file types" })
   end
 }
--- lua/ivan/plugins/telescope.lua
+++ lua/ivan/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     "nvim-telescope/telescope-ui-select.nvim",
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
   },
   config = function()
     local telescope = require("telescope")
@@ -20,7 +21,7 @@ return {
             -- ["<C-_>"] = require('telescope.actions.layout').toggle_preview,
             -- ["<C-?>"] = actions.which_key,
 
-            -- ["<C-p>"] = require('telescope.actions.layout').toggle_preview,
+            ["<C-p>"] = require('telescope.actions.layout').toggle_preview,
             -- ["<C-n>"] = require('telescope.actions.layout').cycle_layout_next,
             -- ["<C-p>"] = require('telescope.actions.layout').cycle_layout_prev,
             ["<C-j>"] = actions.move_selection_next,
@@ -32,9 +33,33 @@ return {
         ["ui-select"] = {
           themes.get_dropdown({}),
         },
+        fzf = {
+          fuzzy = true,                    -- false will only do exact matching
+          override_generic_sorter = true,  -- override the generic sorter
+          override_file_sorter = true,     -- override the file sorter
+          case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
+        }
       },
     })
 
     telescope.load_extension("ui-select")
+    telescope.load_extension('fzf')
+
+    -- local buffers = function()
+    --   require('telescope.builtin').buffers({ previewer = false })
+    -- end
+    -- vim.keymap.set("n", ",m", buffers, { desc = "Fuzzy find recent files" })
+    vim.keymap.set("n", ",m", "<cmd>Telescope oldfiles previewer=false<cr>", { desc = "Fuzzy find recent files" })
+    vim.keymap.set("n", ",b", "<cmd>Telescope buffers previewer=false<cr>", { desc = "Show open buffers" })
+
+    vim.keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "[F]uzzy find [k]eymaps" })
+
+    -- vim.keymap.set("n", "<leader>fc", "<cmd>Telescope colorscheme enable_preview=true<cr>", { desc = "[F]uzzy find [c]olorschemes" })
+    -- vim.keymap.set("n", "<Leader>ft", "<cmd>Telescope filetypes<cr>", { desc = "[F]uzzy find file [t]ypes" })
+    -- vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "[F]uzzy find [f]iles in cwd" })
+    -- keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "[F]ind [s]tring in cwd" })
+    -- keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "[F]ind string under [c]ursor in cwd" })
+
   end
 }
