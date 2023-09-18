b50429118bae65ed341a3584afbb70427a84bb64
Ivan Alejandro <ivanalejandro0@gmail.com>
2023-09-18 13:56:44 -0300

    Replace comment plugin

---
+++ lua/ivan/plugins/comment.lua
@@ -0,0 +1,11 @@
+return {
+  'numToStr/Comment.nvim',
+  config = function ()
+    require('Comment').setup()
+    -- map muscle memory from tcomment_vim
+    vim.keymap.set("n", "<C-_><C-_>", "<Plug>(comment_toggle_linewise_current)", { desc = "Comment line" })
+    -- vim.keymap.set("v", "<C-_><C-_>", "<Plug>(comment_toggle_blockwise_visual)", { desc = "Comment lines" })
+    vim.keymap.set("x", "<C-_><C-_>", "<Plug>(comment_toggle_linewise_visual)", { desc = "Comment lines" })
+  end,
+  lazy = false,
+}
