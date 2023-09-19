2dd774804aa0d508a2ec6cf0d075fdb28fbeb15e
Ivan Alejandro <ivanalejandro0@gmail.com>
2023-09-19 18:43:37 -0300

    Use my own diagnostic maps

--- lua/ivan/plugins/lsp.lua
+++ lua/ivan/plugins/lsp.lua
@@ -52,8 +52,10 @@ return {
 
     -- Global mappings.
     -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+    -- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+    -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+    vim.keymap.set('n', '<C-k>', function() vim.diagnostic.goto_prev({ wrap = false }) end)
+    vim.keymap.set('n', '<C-j>', function() vim.diagnostic.goto_next({ wrap = false }) end)
     -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
     -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
