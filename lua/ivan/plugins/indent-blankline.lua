3679a145179b20754be44d8cc2a24f80d089db1a
Ivan Alejandro <ivanalejandro0@gmail.com>
2023-10-07 23:58:15 -0300

    Update indent-blankline and its new config format

--- lua/ivan/plugins/indent-blankline.lua
+++ lua/ivan/plugins/indent-blankline.lua
@@ -1,9 +1,10 @@
 return {
   -- Add indentation guides even on blank lines
   'lukas-reineke/indent-blankline.nvim',
+  main = "ibl",
   opts = {
-    char = '┊',
-    show_trailing_blankline_indent = false,
-    show_current_context = true,
+    indent = {
+      char = '┊',
+    },
   },
 }
