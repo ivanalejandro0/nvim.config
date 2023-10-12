1e06c7d3d939fd7013ce087dc1f96e2d329a8762
Ivan Alejandro <ivanalejandro0@gmail.com>
2023-10-12 00:58:03 -0300

    Add more info about LanguageTool workaround

--- lua/ivan/plugins/grammarous.lua
+++ lua/ivan/plugins/grammarous.lua
@@ -1,7 +1,13 @@
 return {
   "rhysd/vim-grammarous",
   config = function()
-    -- see https://github.com/rhysd/vim-grammarous/issues/110
+    -- LanguageTool version 6.0:
+    -- "The `--api` parameter for the command-line version has been removed.
+    -- It had long been deprecated and replaced by `--json`."
+    -- See: https://github.com/languagetool-org/languagetool/blob/v6.0/languagetool-standalone/CHANGES.md#60-released-2022-12-29
+    --
+    -- This plugin didn't make the change, so LanguageTool 5.9 (that still has
+    -- the flag) needs to be used. See https://github.com/rhysd/vim-grammarous/issues/110
     vim.g["grammarous#jar_url"] = 'https://www.languagetool.org/download/LanguageTool-5.9.zip'
 
     -- vim.keymap.set("n", "[g", ":<C-u>call grammarous#info_win#action_previous_error()<CR>")
