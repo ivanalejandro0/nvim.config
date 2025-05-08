9dc796a8218adc080276c8193f245cbe9b1a9fb7
Ivan Alejandro <ivanalejandro0@gmail.com>
2025-05-08 17:03:00 -0300

    Pin Telescope to the branch

--- lua/ivan/plugins/telescope.lua
+++ lua/ivan/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.2', -- or
-  -- , branch = '0.1.x',
+  -- tag = '0.1.8', -- or
+  branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     "nvim-telescope/telescope-ui-select.nvim",
