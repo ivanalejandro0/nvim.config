1b91732aea11436a1cd4c82ba8ebb753cc36315e
Ivan Alejandro <ivanalejandro0@gmail.com>
2025-05-16 12:00:50 -0300

    Better looking diagnostics

---
+++ lua/ivan/diagnostics.lua
@@ -0,0 +1,11 @@
+vim.diagnostic.config {
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = 'if_many',
+  },
+  virtual_text = {
+    source = 'if_many',
+    spacing = 2,
+  },
+}
