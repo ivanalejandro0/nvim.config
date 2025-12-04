3182591f2de2dd5a01739c9da10a92f7adc7a525
Ivan Alejandro <ivanalejandro0@gmail.com>
2025-12-04 14:27:26 -0300

    Add keymaps to stage/unstage code

--- lua/ivan/plugins/gitsigns.lua
+++ lua/ivan/plugins/gitsigns.lua
@@ -1,6 +1,23 @@
 return {
   'lewis6991/gitsigns.nvim',
   config = function()
-    require('gitsigns').setup()
+    require('gitsigns').setup({
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map('n', '<space>hs', gitsigns.stage_hunk)
+
+        -- map({'n', 'v'}, '<space>hs', function()
+        map('v', '<space>hs', function()
+          gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        end)
+      end
+    })
   end
 }
