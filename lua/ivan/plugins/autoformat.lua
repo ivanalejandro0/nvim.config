404dc2d0109b7fcef1b3f777d8d06c7054cf037e
Ivan Alejandro <ivanalejandro0@gmail.com>
2026-06-08 14:28:44 -0300

    Properly pass opts to setup function

--- lua/ivan/plugins/autoformat.lua
+++ lua/ivan/plugins/autoformat.lua
@@ -1,36 +1,5 @@
 return {
   "stevearc/conform.nvim",
-  config = function()
-    vim.api.nvim_create_user_command("FormatDisable", function(args)
-      if args.bang then
-        -- FormatDisable! will disable formatting just for this buffer
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
-    end, {
-      desc = "Disable autoformat-on-save",
-      bang = true,
-    })
-    vim.api.nvim_create_user_command("FormatEnable", function()
-      vim.b.disable_autoformat = false
-      vim.g.disable_autoformat = false
-    end, {
-      desc = "Re-enable autoformat-on-save",
-    })
-  end,
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  keys = {
-    {
-      "<leader>f",
-      function()
-        require("conform").format({ async = true, lsp_format = "fallback" })
-      end,
-      mode = "",
-      desc = "[F]ormat buffer",
-    },
-  },
   opts = {
     notify_on_error = false,
     -- TODO: maybe add a whitelist of files I want to format on save, like lua
@@ -61,6 +30,42 @@ return {
       -- You can use 'stop_after_first' to run the first available formatter from the list
       javascript = { "prettierd", "prettier", stop_after_first = true },
       typescript = { "prettierd", "prettier", stop_after_first = true },
+
+      sql = { "sqlfluff" },
+    },
+  },
+
+  config = function(_, opts)
+    require("conform").setup(opts)
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+  end,
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format({ async = true, lsp_format = "fallback" })
+      end,
+      mode = "",
+      desc = "[F]ormat buffer",
     },
   },
 }
